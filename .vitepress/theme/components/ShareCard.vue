0649cb9a55b7c32bd421a408c3867b895239d5e5
salvor <yinmingzhuo@gmail.com>
2023-04-03 15:34:07 +0800

    new blog init

---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>Hi, I'm ymz 👋</span>
+    </h1>
+    <p class="description">
+      A normal student who likes super mario.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+  font-weight: bold;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
---
+++ index.md
@@ -0,0 +1,6 @@
+---
+layout: home
+date: 2023-04-03
+title: home
+sidebar: false
+---
